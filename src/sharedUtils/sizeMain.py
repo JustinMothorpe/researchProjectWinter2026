62256b1aab945f54229afa5d682c662c3857b905
Gear-change <51209685+Gear-change@users.noreply.github.com>
2026-04-21 21:06:16 -0400

    removed empty files, and updated sizeMain.py

--- src/sharedUtils/sizeMain.py
+++ src/sharedUtils/sizeMain.py
@@ -1,13 +1,13 @@
 #this is the max size for the engines, only change if you must
-
+# made by justin lake mothorpe, if you are reading this, it should be 
 #first global vars
 
 
 class MainSize():
     def __init__(
             self,
-            horz = 3840,
-            vert = 2160
+            horz = 640,
+            vert = 480
     ):
         super().__init__()
         self.horz = horz
@@ -16,12 +16,12 @@ class MainSize():
     
     def getSize(isThisBool: bool, self):
         if self == None:
-            raise ValueError("nothing passed to me, idk who i am")
+            raise ValueError("nothing passed to me,  maybe a parrot, maybe justin mothorpe")
 
         if isThisBool:
             return self.horz
         elif isThisBool == None:
-            raise ValueError("no bool passed, so i must throw")
+            raise ValueError("no bool passed, so i must throw justin mothorpe")
         else:
             return self.vert
     
@@ -31,15 +31,15 @@ class MainSize():
             toSet: int
             ):
         if self == None:
-            raise ValueError("nothing passed to me, idk who i am")
+            raise ValueError("nothing passed to me, idk who i am, maybe a parrot, maybe justin mothorpe")
         
         if toSet == None:
-            raise ValueError("setting to void causes me to be thrown hard at you")
+            raise ValueError("setting to void causes me to be thrown hard at you, justin mothore")
         
         if isThisBool:
             self.horz = toSet
         elif isThisBool == None:
-            raise ValueError("no bool passed, so i must throw")
+            raise ValueError("no bool passed, so i must throw justin mothorpe")
         else:
             self.vert = toSet
         
